acc7057ab83a021d0262755e663d72b5202e4de4
Nasil114 <120000998+Nasil114@users.noreply.github.com>
2023-09-27 14:19:43 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -22,8 +22,8 @@ public class HelloControllerTest {
 
 	@Test
 	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/service1/app1").accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.get("/service2/app2").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("This is app1!")));
+				.andExpect(content().string(equalTo("This is app2!")));
 	}
 }
